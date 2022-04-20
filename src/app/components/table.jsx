aaab3b378254f33a0bd27b88b3322a98f7ba1190
dgl <mykulych15@gmail.com>
2022-04-20 11:10:43 +0300

    task-5

---
+++ src/app/components/table.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import TableHeader from './tableHeader'
+import TableBody from './tableBody'
+
+const Table = ({ children, selectedSort, onSort, columns, data }) => {
+    return (
+        <div className="table">
+            {children || (
+                <>
+                    <TableHeader
+                        selectedSort={selectedSort}
+                        columns={columns}
+                        onSort={onSort}
+                    />
+                    <TableBody columns={columns} data={data} />
+                </>
+            )}
+        </div>
+    )
+}
+
+Table.propTypes = {
+    children: PropTypes.array,
+    selectedSort: PropTypes.object,
+    onSort: PropTypes.func,
+    columns: PropTypes.object,
+    data: PropTypes.array
+}
+
+export default Table
